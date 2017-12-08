9d7e5101b2ca4dd428b8a7412cc4aac5cf4e86a3
sjyf <songjyf@126.com>
2017-12-08 13:06:05 +0800

    add new files

---
+++ python_for_you_me/testhundred.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+number = int(input("Enter an integer: "))
+if number <= 100:
+	print("Your number is smaller and equal to 100")
+else:
+	print("Your number is greater than 100")
