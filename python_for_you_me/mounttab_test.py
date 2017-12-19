86264e8ceda0de9da2acf4c3c6963c49f75fadca
sjyf <songjyf@126.com>
2017-12-19 17:36:15 +0800

    test is half done

---
+++ python_for_you_me/mounttab_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env	python3
+import unittest
+from mounttab import parse_mounts
+
+class TestMount(unittest.TestCase):
+	def test_parsemount(self):
+		result = parse_mounts()
+		self.assertIsInstance(result, list)
+		self.assertIsInstance(result[0], tuple)
+
+	def test_roottext4(self):
+		result = parse_mounts()
+		for line in result:
+			if line[1] == '/' and line[2] != 'rootfs':
+				self.assertEqual(line[2], 'ext4')
+
+if __name__ == '__main__':
+	unittest.main()
+
